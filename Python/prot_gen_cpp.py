682a85e34008af66f0279e15bedbbbf6abfb9391
rtek1000 <rh.silicon@gmail.com>
2024-03-20 02:56:29 -0300

    Update prot_gen_cpp.py

--- Python/prot_gen_cpp.py
+++ Python/prot_gen_cpp.py
@@ -13,6 +13,8 @@
 - - The keyword does not need to be complete (uint: uint8_t, uint32_t)
 - - The source file name must be placed after the script name: prot_gen_cpp.py main.cpp
 - - Long line wrapping functions are not supported
+- - The function must be aligned in the first column, use VScode's automatic indentation before generating the prototypes.
+- - - VScode auto indent shortcut: Control + Shift + I
 
 - Referece: https://cplusplus.com/doc/tutorial/variables/
 """
