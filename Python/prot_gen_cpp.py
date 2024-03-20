8bb24001134e90b325f4a03657579bed7290c2a8
rtek1000 <rh.silicon@gmail.com>
2024-03-20 07:11:53 -0300

    Update prot_gen_cpp.py
    Info update

--- Python/prot_gen_cpp.py
+++ Python/prot_gen_cpp.py
@@ -13,19 +13,19 @@
     - - prot_gen_cpp.py main.cpp
     - - prot_gen_cpp.py scketch.ino
 
-    - Rules:
+    Rules:
+    - The first word on the line must be listed in the 'header_array' variable (void, uint, String etc)
+    - - The keyword does not need to be complete (uint: uint8_t, uint32_t)
 
-    - - The first word on the line must be listed in the 'header_array' variable (void, uint, String etc)
-    - - - The keyword does not need to be complete (uint: uint8_t, uint32_t)
+    - Function prototypes with multiple lines are supported
 
-    - - Function prototypes with multiple lines are supported
+    - Prototypes already present in the code are not listed
 
-    - - Prototypes already present in the code are not listed
-
-    - - The function must be aligned in the first column
-    - - - Use automatic indentation before generating the prototypes:
-    - - - - VScode auto indent shortcut: Control + Shift + I
-    - - - - Arduino IDE auto indent shortcut: Control + T
+    - The function must be aligned in the first column
+    - - Use automatic indentation before generating the prototypes:
+    - - - VScode auto indent shortcut: Control + Shift + I
+    - - - - (Some extensions may interfere with this)
+    - - - Arduino IDE auto indent shortcut: Control + T
 
 - Referece: https://cplusplus.com/doc/tutorial/variables/
 """
