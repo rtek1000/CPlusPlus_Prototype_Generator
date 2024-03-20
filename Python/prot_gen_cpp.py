01d21f3bf5bb775ee80f21ecb6332f1a26485d79
rtek1000 <rh.silicon@gmail.com>
2024-03-20 03:06:33 -0300

    Update prot_gen_cpp.py
    Added example of use with Arduino IDE file (*.ino), the extension does not need to be *.cpp

--- Python/prot_gen_cpp.py
+++ Python/prot_gen_cpp.py
@@ -11,7 +11,9 @@
 - Rules:
 - - The first word on the line must be listed in the header_array variable (void, uint, String etc)
 - - The keyword does not need to be complete (uint: uint8_t, uint32_t)
-- - The source file name must be placed after the script name: prot_gen_cpp.py main.cpp
+- - The source file name must be placed after the script name:
+- - - prot_gen_cpp.py main.cpp
+- - - prot_gen_cpp.py scketch.ino
 - - Long line wrapping functions are not supported
 - - The function must be aligned in the first column, use VScode's automatic indentation before generating the prototypes.
 - - - VScode auto indent shortcut: Control + Shift + I
